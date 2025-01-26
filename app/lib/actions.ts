385c65b0dce04afcdaad8a0f2b925f078e84db39
srm04072024 <s.r.m.20.3.2018@gmail.com>
2025-01-26 19:42:52 +0530

    Add Error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -23,10 +23,15 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
 
-      await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+      try {
+        await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+      } catch (error) {
+        // We'll log the error to the console for now
+        // console.error(error);
+      }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -49,11 +54,16 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    // console.error(error);
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices/');
